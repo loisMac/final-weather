c38531b4d2aecf887fbfe52de61cda8f3316e633
loisMac <155264617+loisMac@users.noreply.github.com>
2024-01-18 16:46:45 +0000

    Added JS to get city and temperature from API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
+}
+
+function searchCity(city) {
+  let apiKey = "08tc66c2dd234aae04o0a7f3aee015bc";
+  let apiUrl = 
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Glasgow");
