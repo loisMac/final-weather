1f04287772f55e959c8981dd269df205bae5690f
loisMac <155264617+loisMac@users.noreply.github.com>
2024-01-22 20:15:35 +0000

    Replaced HTML with displayForecast function

--- src/index.js
+++ src/index.js
@@ -61,7 +61,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+
+forecastHtml = 
+forecastHtml +
+`
+<div class="weather-forecast-day">
+<div class="weather-forecast-date">${day}</div>
+<div class="weather-forecast-icon">⛅️</div>
+<div class="weather-forecast-temperatures">
+<div class="weather-forcast-temperature">
+<strong>15˚</strong>
+</div>
+<div class="weather-forecast-temperature">9˚</div>
+</div>
+</div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Glasgow");
+displayForecast();
